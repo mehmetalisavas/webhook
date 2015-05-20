b9fbf5045a96760867c240720893ea6a35bfa77e
mehmetalisavas <mehmetali.savas@yandex.com>
2015-05-21 02:11:27 +0300

    webhook: test commit list

--- src/main.go
+++ src/main.go
@@ -8,11 +8,23 @@ import (
 
 func main() {
 	http.HandleFunc("/", payload)
+	http.HandleFunc("/repos/mehmetalisavas/webhook/hooks", List)
 	http.ListenAndServe(":4567", nil)
 }
 // payload is created for github webhook
 func payload(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hi, My name is Mehmet Ali ", r.URL.Path[1:])
+	var pay map[string]interface{}
+
+	// json.Unmarshal(r, &pay)
+	json.NewDecoder(r.Body).Decode(&pay)
+	// fmt.Fprintf(w, pay, r.URL.Path[1:])
+	fmt.Println(pay)
+	// fmt.Println(w, pay, r.URL.Path[1:])
+	fmt.Println("SomethingLikeThis")
+}
+
+func List(w http.ResponseWriter, r *http.Request) {
 	var pay map[string]interface{}
 
 	// json.Unmarshal(r, &pay)
