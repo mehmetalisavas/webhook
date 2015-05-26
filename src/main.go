b4e6f96f4657b85d00a305a40ac6fab0930174ee
Mehmet Ali <mehmetali.savas@yandex.com>
2015-05-26 07:02:40 +0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -17,6 +17,7 @@ func payload(w http.ResponseWriter, r *http.Request) {
 	var pay map[string]interface{}
 	// json.Unmarshal(r, &pay)
 	json.NewDecoder(r.Body).Decode(&pay)
+	
 	// fmt.Fprintf(w, pay, r.URL.Path[1:])
 	fmt.Println(pay)
 	// fmt.Println(w, pay, r.URL.Path[1:])
